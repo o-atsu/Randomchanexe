b5ac93696d49b3846ac33b9a744484109638c77a
ATsU <39590619+o-atsu@users.noreply.github.com>
2022-07-16 01:10:39 +0900

    ADD:RandomChan
    RandomChan.VRM導入
    テンプレート：Third Person
    レンダーパイプライン：URP
    Asset：UniVRM-0.101.0

---
+++ Assets/Scripts/static/DATA.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+/*
+PLAYERS:Prefabs/PlayersディレクトリのPrefabを取得
+ENEMIES:Prefabs/EnemiesディレクトリのPrefabを取得
+
+*現在未使用*
+*/
+
+public class DATA{
+    private static List<string> PLAYERS = new List<string>();
+    private static List<string> ENEMIES = new List<string>();
+
+	static string playerpath = "Assets/Resources/Prefabs/Players";
+	static string enemypath = "Assets/Resources/Prefabs/Enemies";
+
+	static DATA(){
+		string[] ps = System.IO.Directory.GetFiles(@playerpath, "*.prefab");
+		string[] es = System.IO.Directory.GetFiles(@enemypath, "*.prefab");
+
+		foreach(string i in ps){
+			PLAYERS.Add(System.IO.Path.GetFileNameWithoutExtension(i));
+
+		}
+		foreach(string i in es){
+			ENEMIES.Add(System.IO.Path.GetFileNameWithoutExtension(i));
+		}
+	}
+
+	// パラメータへのアクセス
+	public static string GetPlayer(int i){ return PLAYERS[i]; }
+	public static string GetEnemy(int i){ return ENEMIES[i]; }
+}
